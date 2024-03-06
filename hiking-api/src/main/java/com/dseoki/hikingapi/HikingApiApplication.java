94b3face3cc2a5ff29abd12e34c32e44b2da2750
dseoki <jeon931207@naver.com>
2024-03-06 14:39:33 +0900

    스프링부트 초기 생성

---
+++ hiking-api/src/main/java/com/dseoki/hikingapi/HikingApiApplication.java
@@ -0,0 +1,13 @@
+package com.dseoki.hikingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikingApiApplication.class, args);
+    }
+
+}
---
+++ hiking-api/src/test/java/com/dseoki/hikingapi/HikingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dseoki.hikingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
